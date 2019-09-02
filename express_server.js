cc4445c0ff50dd260c2999015011e9a564fd050c
plonsdale1 <petex.1995@outlook.com>
2019-09-02 21:09:24 +0000

    third commit of express_server

--- express_server.js
+++ express_server.js
@@ -8,13 +8,16 @@ const urlDatabase = {
 };
 
 app.get("/", (request, response) => {
-  response.send("Hello!")
+  response.send("Hello!");
 });
 
 app.get("/urls.json", (request, response) => {
   response.json(urlDatabase);
-})
+});
 
+app.get("/hello", (request, response) => {
+  response.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
