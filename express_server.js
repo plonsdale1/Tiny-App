344b80ef5e2e5a67d8fc1a991154fe07bb8693f6
plonsdale1 <petex.1995@outlook.com>
2019-09-03 19:42:23 +0000

    made the delete buttons DELETEgit add .!

--- express_server.js
+++ express_server.js
@@ -57,6 +57,14 @@ app.post("/urls", (request, response) => {
   response.redirect(`/urls/${shortUrl}`);
   urlDatabase[shortUrl] = request.body.longURL;
 });
+
+app.post("/urls/:shortURL/delete", (request, response) => {
+  delete urlDatabase[request.params.shortURL];
+  console.log("delete", request.params.shortURL);
+ response.redirect("/urls");
+
+})
+
 const characters = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 function generateRandomString(length, chars) {
    newUrl = '';
