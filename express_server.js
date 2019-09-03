40fbeb48a60a342ef16a0994f1c159fc4dd9e5b9
plonsdale1 <petex.1995@outlook.com>
2019-09-03 00:39:54 +0000

    got the aplhanumeric working

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -9,13 +11,24 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.post("/urls", (request, response) => {
+  console.log(request.body);
+  response.send("OK")
+});
+
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
+});
+
+
 app.get("/urls/:shortURL", (request, response) => {
   let templateVars = {shortURL: request.params.shortUrl, longURL: request.params.shortUrl};
   response.render("urls_show", templateVars);
 })
 
-app.get("/", (request, response) => {
-  response.send("Hello!");
+
+app.get("/urls.json", (request, response) => {
+  response.json(urlDatabase);
 });
 
 app.get("/urls", (request, response) => {
@@ -23,16 +36,24 @@ app.get("/urls", (request, response) => {
   response.render("urls_index", templateVars)
 });
 
-app.get("/urls.json", (request, response) => {
-  response.json(urlDatabase);
-});
+
 
 app.get("/hello", (request, response) => {
   response.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/", (request, response) => {
+  response.send("Hello!");
+});
 
+const characters = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+function generateRandomString(length, chars) {
+  let newUrl = '';
+  for (let i = length; i > 0; --i) newUrl += chars[Math.floor(Math.random() * chars.length)];
+    return newUrl;
+}
 
+console.log(generateRandomString(6, characters));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
